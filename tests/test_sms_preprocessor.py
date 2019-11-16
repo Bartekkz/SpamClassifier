9bda918452e40e6f8a2c223f9a589e9f8ed07f5b
Bearnardd <bartosz.szmelczynski@catbots.pl>
2019-11-16 20:31:56 +0100

    add clena_Text function and chagnge flow of message

--- tests/test_sms_preprocessor.py
+++ tests/test_sms_preprocessor.py
@@ -4,11 +4,17 @@ sys.path.append('..')
 from utils.sms_preprocessor import SmsPreprocessor
 
 
-def test_pre_processor_create_key_word_map():
+
+
+@pytest.mark.parametrize("test_input, expected, clean_text", [
+    ("Hello my dear friend. My Friend told me", {"Hello": 1, "my": 2, "dear": 3, "friend": 4,
+                                                 "My": 5, "Friend": 6, "told": 7,
+                                                 "me": 8}, False),
+    (["Hello what Is going on . What ?", "fuck what"], {"hello": 1, "what": 2, "is": 3, "going": 4, "on": 5, "fuck": 6}, True)
+])
+def test_preprocessor_create_key_word_map(test_input, clean_text, expected):
     pr = SmsPreprocessor(False)
-    text = "Hello my dear friend. My brother told me."
-    key_word_map = pr.create_key_word_dict(text) 
-    expected = {"hello": 1, "my": 2, "dear": 3, "friend": 4, "brother": 5, "told": 6, "me": 7}
+    key_word_map = pr.create_key_word_dict(test_input, clean_text) 
     assert key_word_map == expected 
 
 
@@ -29,13 +35,20 @@ def test_preprocessor_remove_stopwords():
     
 
 
-@pytest.mark.parametrize("test_input, expected, key_word_map",[
+@pytest.mark.parametrize("test_input, expected, key_word_map", [
     ("Hello my dear friend. My borther told me", [1, 2, 3, 4, 2, 5, 6, 7], None),
     ("hello what is going on. What?", [1, 2, 3, 4, 5, 2], {"hello": 1, "what": 2, "is": 3, "going": 4, "on": 5})
 ])
-def test_pre_processor_tokenizer(test_input, expected, key_word_map):
+def test_preprocessor_tokenizer(test_input, expected, key_word_map):
     pr = SmsPreprocessor(False)
     tokenized = pr.tokenize(test_input, key_word_map=key_word_map)
     assert tokenized == expected 
 
 
+@pytest.mark.parametrize("test_input, expected", [
+    (["Hello my Friend?!", "What. is GOing on"], ["hello my friend", "what is going on"])
+])
+def test_preprocessor_clean_text(test_input, expected):
+    pr = SmsPreprocessor(False)
+    cleaned = pr.clean_text(test_input)
+    assert cleaned == expected
