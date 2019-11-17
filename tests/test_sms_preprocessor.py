1c42ee9ad9358b644be89d339f56b2951c180e57
Bearnardd <bartosz.szmelczynski@catbots.pl>
2019-11-17 03:24:32 +0100

    fix tests formulas

--- tests/test_sms_preprocessor.py
+++ tests/test_sms_preprocessor.py
@@ -5,39 +5,39 @@ from utils.sms_preprocessor import SmsPreprocessor
 
 
 
-
-@pytest.mark.parametrize("test_input, expected, clean_text", [
-    ("Hello my dear friend. My Friend told me", {"Hello": 1, "my": 2, "dear": 3, "friend": 4,
-                                                 "My": 5, "Friend": 6, "told": 7,
-                                                 "me": 8}, False),
-    (["Hello what Is going on . What ?", "fuck what"], {"hello": 1, "what": 2, "is": 3, "going": 4, "on": 5, "fuck": 6}, True)
+@pytest.mark.parametrize("test_input, expected ", [
+    ("Hello my dear friend. My Friend told me", {"Hello": 1, "my": 2, "dear": 3, "friend.": 4,
+                                                 "My": 5, "Friend": 6, "told": 7, "me": 8}),
+    (["Hello what Is going on What", "fuck what"], {"Hello": 1, "what": 2, "Is": 3, "going": 4,
+                                                    "on": 5, "What": 6, "fuck": 7})
 ])
-def test_preprocessor_create_key_word_map(test_input, clean_text, expected):
+def test_preprocessor_create_key_word_map(test_input, expected):
     pr = SmsPreprocessor(False)
-    key_word_map = pr.create_key_word_dict(test_input, clean_text) 
+    key_word_map = pr.create_key_word_dict(test_input) 
     assert key_word_map == expected 
 
 
 def test_preprocessor_preprocess():
     pr = SmsPreprocessor(False)
     text = "Hello what is going?! NOthing."
-    tokenized = pr.preprocess(text)
+    tokenized = pr.preprocess(text, False)
     expected = 'hello what is going nothing'
     assert tokenized == expected
 
-
-def test_preprocessor_remove_stopwords():
+@pytest.mark.parametrize("test_input, expected", [
+    (["hello is she", "you are good"], ["hello", "good"]),
+    ("hello is she", "hello")
+])
+def test_preprocessor_remove_stopwords(test_input, expected):
     pr = SmsPreprocessor(False)
-    text = ["hello is she", "you are good"]
-    cleaned_text = pr.remove_stopwords(text)
-    expected = ["hello", "good"]
+    cleaned_text = pr.remove_stopwords(test_input)
     assert cleaned_text == expected
     
 
 
 @pytest.mark.parametrize("test_input, expected, key_word_map", [
-    ("Hello my dear friend. My borther told me", [1, 2, 3, 4, 2, 5, 6, 7], None),
-    ("hello what is going on. What?", [1, 2, 3, 4, 5, 2], {"hello": 1, "what": 2, "is": 3, "going": 4, "on": 5})
+    ("Hello my dear friend. My borther told me", [1, 2, 3, 4, 5, 6, 7, 8], None),
+    ("hello what is going on. What?", [1, 2, 3, 4, 5, 6], {"hello": 1, "what": 2, "is": 3, "going": 4, "on.": 5, "What?": 6})
 ])
 def test_preprocessor_tokenizer(test_input, expected, key_word_map):
     pr = SmsPreprocessor(False)
