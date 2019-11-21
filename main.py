91173f8ac4d2febfcf6292e9b4ef6ce3c71f4296
bartekkz <bartek.szmelczynski@gmail.com>
2019-11-21 23:51:33 +0100

    testing pipelines

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from utils.sms_preprocessor import SmsPreprocessor
 from utils.tokenizer import Tokenizer
 from sklearn.pipeline import Pipeline
@@ -6,17 +8,17 @@ from sklearn.pipeline import Pipeline
 
 text = ["Hello man what do you want? what is going on", 'what do going You want']
 
-pipeline = Pipeline([
-    ("preprocessor", SmsPreprocessor(False)),
-    ("tokenizer", Tokenizer())
-])
 
 
 if __name__ == '__main__':
+    pipeline = Pipeline([
+        ("preprocessor", SmsPreprocessor(False)),
+        ("tokenizer", Tokenizer())
+    ])
     tokenized, key_word_map = pipeline.fit_transform(text)
-    print(tokenized)
-    print(key_word_map)
-
+    token, _ = pipeline.fit_transform("Barek")
+    print(token)
+    print(_)
 
 
 
