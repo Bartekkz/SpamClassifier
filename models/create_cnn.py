f27ca838c667ae14c3982b909c3df90ad9f509e5
bartekkz <bartek.szmelczynski@gmail.com>
2019-11-22 22:55:12 +0100

    add max_pooling param to convolution layer creator and init build function

--- models/create_cnn.py
+++ models/create_cnn.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+from keras.layers import Conv2D
+from keras.models import Sequential
 
 
-def convolutional_layer(filters, kernel_size, strides, padding, data_format):
+def convolutional_layer_with_pooling(filters, kernel_size, strides, padding, data_format=None, pooling='max'):
   '''
   creates Keras based convolutional layer
   
@@ -9,7 +12,24 @@ def convolutional_layer(filters, kernel_size, strides, padding, data_format):
   :param strides: int or tuple/list of 2 int's -> the height and width of the convolution
   :param padding: string: type of padding
   :param data_format: order of the dimensions in the inputs
+  :param pooling: type of pooling
   
   :returns: keras.layers.Conv2d
   : #TODO: add error raising
   '''
+  conv_layer = Conv2D(filters=filters,
+                      kernel_size=kernel_size,
+                      strides=strides,
+                      padding=padding,
+                      data_format=dataformat)
+
+
+def build_convolutional_model(filters, kernel_size, padding, data_format, classes, **kwargs):
+  layers = kwargs.get('layers', 1)
+  dropout = kwargs.get('dropout', 0)
+  fc1 = kwargs.get('fc1', False)
+  loss_l2 = kwargs.get('loss_l2', 0)
+  lr = kwargs.get('lr', 0.001)
+
+  print('Creating model...')
+  model = Sequential()
