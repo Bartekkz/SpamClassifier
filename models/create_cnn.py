8c7eaa7b129550cbfb02b279cecb5deda0bd0a4e
bartekkz <bartek.szmelczynski@gmail.com>
2019-11-23 13:02:14 +0100

    update todo list

--- models/create_cnn.py
+++ models/create_cnn.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-from keras.layers import Conv2D
+from tenkeras.layers import Conv2D
 from keras.models import Sequential
 
 
-def convolutional_layer_with_pooling(filters, kernel_size, strides, padding, data_format=None, pooling='max'):
+def convolutional_layer_with_pooling(model, 
+                                     filters, 
+                                     kernel_size, 
+                                     strides, padding, 
+                                     data_format=None, 
+                                     pooling='max', **kwargs)
   '''
   creates Keras based convolutional layer
-  
+
   :param filters: int -> nubmer of output filters in convolution
   :param kernel_size: int or tuple/list of 2 int's -> the height and width of the 2D convolution window
   :param strides: int or tuple/list of 2 int's -> the height and width of the convolution
@@ -17,12 +22,17 @@ def convolutional_layer_with_pooling(filters, kernel_size, strides, padding, dat
   :returns: keras.layers.Conv2d
   : #TODO: add error raising
   '''
+
+  
+
   conv_layer = Conv2D(filters=filters,
                       kernel_size=kernel_size,
                       strides=strides,
                       padding=padding,
-                      data_format=dataformat)
+                      data_format=data_format)
 
+  #TODO: add with name_scope, convert to tf.keras 
+  #with K.name_scope  
 
 def build_convolutional_model(filters, kernel_size, padding, data_format, classes, **kwargs):
   layers = kwargs.get('layers', 1)
