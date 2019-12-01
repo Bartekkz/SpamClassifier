4f89596fd8e152639ed3d7447d52854e9ff36f01
Bartosz Szmelczynski <43574448+Bartekkz@users.noreply.github.com>
2019-12-01 13:51:45 +0100

    import and load convolution model

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ from utils.data_loader import DataLoader
 from utils.sms_preprocessor import SmsPreprocessor
 from utils.tokenizer import Tokenizer
 from sklearn.pipeline import Pipeline
-
+from models.create_cnn import build_convolutional_model
 
 text = ["Hello man what do you want? what is going on", 'what do going You want']
 
 if __name__ == '__main__':
     pipeline = Pipeline([
-        ("preprocessor", SmsPreprocessor(False)),
+        ("preprocessor", SmsPreprocessor(True)),
         ("tokenizer", Tokenizer())
     ])
     loader = DataLoader('./data/sms_data')
     tokenized, key_word_map = pipeline.fit_transform(loader.sms_data)
-    print(tokenized[100:120])
-  
-
-
-
+    model = build_convolutional_model(filters=32, kernel_size=3, padding="valid", strides=1, data_format=None,
+                                      classes=2, layers=3, fc1=True, fc_dropout=0.5, pooling='max', pool_size=2)
+    
+    print(model.summary())
 
