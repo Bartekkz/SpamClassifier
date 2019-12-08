e3b61373faf21505f1850a8de3a2c077f9d1db5b
bartekkz <bartek.szmelczynski@gmail.com>
2019-12-08 03:02:25 +0100

    add new method to calculate class weights

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from models.train import train_model
 
 import numpy as np
 import argparse
+from sklearn.utils import class_weight
 
 parser = argparse.ArgumentParser()
 feature_parser = parser.add_mutually_exclusive_group(required=False)
@@ -16,13 +17,6 @@ feature_parser.add_argument('--no-save', dest='save', action='store_false')
 parser.set_defaults(save=True)
 
 args = parser.parse_args()
-text = ["Hello man what do you want? what is going on", 'what do going You want']
-class_weight = {
-    0: 1.,
-    1: 10.0 
-}
-
-
 
 if __name__ == '__main__':
     pipeline = Pipeline([
@@ -33,9 +27,14 @@ if __name__ == '__main__':
     tokenized, key_word_map = pipeline.fit_transform(loader.sms_data)
     labels = loader.labels
 
+
+    class_weight = class_weight.compute_class_weight('balanced',
+                                                     np.unique(labels),
+                                                     labels)
+
     model = build_convolutional_model(filters=32, kernel_size=3, padding="valid", strides=1, data_format=None,
                                       classes=2, layers=3, fc=True, fc_dropout=0.5, pooling='max', pool_size=2)
-
+    # TODO: add parameters to the function instead of hard coded 
     model = train_model(model=model, 
                         X=tokenized,
                         y=labels, 
@@ -45,4 +44,5 @@ if __name__ == '__main__':
                         epochs=4,
                         batch_size=16,
                         class_weight=class_weight,
-                        conv_dropout=True)
+                        conv_dropout=True,
+                        shuffle_data=True)
