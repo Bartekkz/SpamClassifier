c63b5496e9e7d42c659111d5a6a8dab5a1ed9b19
Bartosz Szmelczynski <43574448+Bartekkz@users.noreply.github.com>
2019-12-08 14:46:13 +0100

    finally fix model paths

--- models/train.py
+++ models/train.py
@@ -2,7 +2,7 @@ import os
 from sklearn.utils import shuffle
 
 
-def train_model(model, X, y, shuffle_data, save_model, **kwargs):
+def train_model(model, X, y, save_model, shuffle_data=True, **kwargs):
     """
     Trains the model with given params and saves the model architecture
     to json file, and weights to hdf5 format
@@ -10,7 +10,7 @@ def train_model(model, X, y, shuffle_data, save_model, **kwargs):
     :param X: array or list -> data
     :param y: array or list -> labels for given data
     :param save_model: bool -> save model architecture and weights for later reproduction
-    :param path: str -> directory for saving model (ends with .json), if You want to save model and weights to
+    :param shuffle_data: bool -> random shuffle data
     different directories You can omit this parameter and use "model_path" and "weights_path"
     :param kwargs:
     :return: Model
@@ -20,11 +20,11 @@ def train_model(model, X, y, shuffle_data, save_model, **kwargs):
     batch_size = kwargs.get('batch_size', 16)
     class_weight = kwargs.get('class_weight', None)
     validation_split = kwargs.get('validation_split', 0.2)
-    model_path = kwargs.get('model_path', 'model_json.json')
-    weights_path = kwargs.get('weights_path', 'model_weights.h5')
-    data_path = os.path.abspath('data')
+    model_path = kwargs.get('model_data', 'model_json.json')
+    weights_path = kwargs.get('weights_data', 'model_weights.h5')
 
     # TODO: fix paths
+    data_path = os.path.abspath('data')
     model_path = os.path.join(data_path, model_path)
     weights_path = os.path.join(data_path, weights_path)
 
@@ -33,7 +33,6 @@ def train_model(model, X, y, shuffle_data, save_model, **kwargs):
 
     if shuffle_data:
         X, y = shuffle(X, y)
-
 
     model.fit(X, y, epochs=epochs, batch_size=batch_size, validation_split=validation_split, class_weight=class_weight, shuffle=shuffle)
     model_json = model.to_json()
@@ -50,6 +49,5 @@ def train_model(model, X, y, shuffle_data, save_model, **kwargs):
             raise FileExistsError
         else:
             model.save_weights(weights_path)
-
     return model
 
