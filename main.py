bfb10d55968b15a3a274607ab15b20d312c3020c
Bartosz Szmelczynski <43574448+Bartekkz@users.noreply.github.com>
2019-12-14 17:14:53 +0100

    add logging module

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from utils.data_loader import DataLoader
 from utils.sms_preprocessor import SmsPreprocessor
 from utils.tokenizer import Tokenizer
 from models.utils import load_model, predict
-from sklearn.pipeline import Pipeline
-from models.create_cnn import build_convolutional_model 
+from utils.helper_functions import create_pipeline
+from models.create_cnn import build_convolutional_model
 from models.train import train_model
 
 import numpy as np
+from sklearn.pipeline import Pipeline
 import argparse
 from sklearn.utils import class_weight
 import json
 import pickle
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 parser = argparse.ArgumentParser()
 feature_parser = parser.add_mutually_exclusive_group(required=False)
@@ -22,50 +26,41 @@ parser.set_defaults(save=True)
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    with open('data/key_word_map.pkl', 'rb') as f:
-        key_word_map = pickle.load(f)
-    pipeline = Pipeline([
-        ("preprocessor", SmsPreprocessor()),
-        ("tokenizer", Tokenizer(key_word_map=key_word_map))
-    ])
+    loader = DataLoader('data/sms_data')
+    pipeline = create_pipeline(key_word_path="data/key_word_map.pkl")
+
     text = ['Press this button to win 500 dollats', 'I will be late today. Do not wait for me honey',
             'REMINDER FROM O2: To get 2.50 pounds free call credit and details of great offers pls' 
             'reply 2 this text with your valid name, house no and postcode',
             'To earn 1000 dollars send your valid name']
-    model = load_model('data/models_data/model_2.json', 'data/models_data/model_weights_2.h5')
-    predict(text, model, pipeline)
+    #Emodel = load_model('data/models_data/model_2.json', 'data/models_data/model_weights_2.h5')
+    #predict(text, model, pipeline)
 
+    class_weight = class_weight.compute_class_weight('balanced',
+                                                     np.unique(loader.labels),
+                                                     loader.labels)
+    # this class wieghts have better accuracy than the one computed with sklearn
+    class_weight = [1., 1.5]
+    tokenized, key_word_map = pipeline.fit_transform(loader.sms_data)
 
-    #loader = DataLoader('data/sms_data')       
-    #sms_data = loader.sms_data
-    #labels = loader.labels  
+    model = build_convolutional_model(filters=32, kernel_size=3, padding="valid", strides=1,
+                                      data_format=None, classes=2, layers=3, fc=True, fc_dropout=0.5,
+                                      pooling='max', pool_size=2, conv_dropout=True)
 
-    #class_weight = class_weight.compute_class_weight('balanced',
-    #                                                 np.unique(labels),
-    #                                                 labels)
+    #loss: 0.0124 - acc: 0.9971 - binary_accuracy: 0.9971 - val_loss: 0.1277 - val_acc: 0.9525 - val_binary_accuracy: 0.9525class_weight=class_weight
+    #loss: 0.0182 - acc: 0.9946 - binary_accuracy: 0.9946 - val_loss: 0.1411 - val_acc: 0.9632 - val_binary_accuracy: 0.9632
+    #loss: 0.0095 - acc: 0.9973 - binary_accuracy: 0.9973 - val_loss: 0.1279 - val_acc: 0.9686 - val_binary_accuracy: 0.9686
 
-    #tokenized, _ = pipeline.transform(sms_data)
-
-    #model = build_convolutional_model(filters=32, kernel_size=3, padding="valid", strides=1, 
-    #                                  data_format=None, classes=2, layers=3, fc=True, fc_dropout=0.5, 
-    #                                  pooling='max', pool_size=2)
-    #print(model.summary())    
-
-    #model = train_model(model=model, 
-    #                    X=tokenized,
-    #                    y=labels, 
-    #                    save_model=args.save, 
-    #                    model_path='model_2.json',
-    #                    weights_path='model_weights_2.h5',
-    #                    epochs=4,
-    #                    batch_size=16,
-    #                    class_weight=class_weight,
-    #                    conv_dropout=True,
-    #                    shuffle_data=True)
+    model = train_model(model=model,
+                        X=tokenized,
+                        y=loader.labels,
+                        save_model=args.save,
+                        model_path='model_2.json',
+                        weights_path='model_weights_2.h5',
+                        epochs=4,
+                        batch_size=16,
+                        class_weight=class_weight,
+                        shuffle_data=True)
     
-    
-    
-    #predict(["I will be at home home around 5pm"], model, pipeline)
-    
-    
+
     # TODO: change pipeline, sae key_wor_map to the file
