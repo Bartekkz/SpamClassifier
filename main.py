4933757c53ee53491b668e7429558d6f5ad36a88
Bartosz Szmelczynski <43574448+Bartekkz@users.noreply.github.com>
2019-12-22 18:00:21 +0100

    clean up project
    clean up directories, remove old, no used files, etc.

--- main.py
+++ main.py
@@ -26,22 +26,12 @@ parser.set_defaults(save=True)
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    #loader = DataLoader('data/sms_data')
+    loader = DataLoader('data/sms_data')
     pipeline = create_pipeline(key_word_path="data/pickled/key_word_map_new_data_1.pkl")
 
-    text = ['Press this button to win 500 dollars', 'I will be late today. Do not wait for me honey',
-            'REMINDER FROM O2: To get 2.50 pounds free call credit and details of great offers pls' 
-            'reply 2 this text with your valid name, house no and postcode',
-            'To earn 1000 dollars send your valid name',
-            'If you want to win a car send your vaild email']
-    """
-    class_weight = class_weight.compute_class_weight('balanced',
-                                                     np.unique(loader.labels),
-                                                     loader.labels)
-    """
     # this class wieghts have better accuracy than the one computed with sklearn
     class_weight = [1., 1.5]
-    """
+
     tokenized, key_word_map = pipeline.fit_transform(loader.sms_data)
 
     model = build_convolutional_model(filters=32, kernel_size=3, padding="valid", strides=1,
@@ -58,7 +48,5 @@ if __name__ == '__main__':
                         batch_size=16,
                         class_weight=class_weight,
                         shuffle_data=True)
-    """
-    model = load_model('data/models_data/model_conv_drop_false_15_new_data_1.json', 'data/models_data/model_weights_conv_drop_false_15_new_data_1.h5')
-    predict(text, model, pipeline)    
+
 
